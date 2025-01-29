6abf52a11c21265859e7e94c78e4173da2854e99
Nicolas Paiuca Buscarini <nicolasbuscarini@hotmail.com>
2025-01-29 08:47:18 -0300

    INIT: Add .editorconfig, solution file, and EF Core base classes
    - Add or modify .editorconfig to enforce C# coding conventions
    - Add Visual Studio solution file (Data.Context.Package.sln)
    - Include GitHub Actions workflow for NuGet package publishing
    - Create Data.Context.Package.csproj with project properties
    - Add BaseDbContext and BaseIdentityDbContext classes
    - Add BaseEntity abstract class and IBaseEntity interface
    - Implement BaseRepository class and IBaseRepository interface
    - Configure CommandFailure entity with CommandFailureConfiguration
    - Provide DatabaseConfig class for SQL Server configuration
    - Introduce CommandFailure class to log failed command executions

---
+++ src/Data.Context.Package/Base/Context/BaseDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CodeFlow.Data.Context.Package.Configuration;
+using CodeFlow.Data.Context.Package.Tracking;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFlow.Data.Context.Package.Base.Context;
+
+public class BaseDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<CommandFailure> CommandFailures { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfiguration(new CommandFailureConfiguration());
+    }
+}
---
+++ src/Data.Context.Package/Base/Context/BaseIdentityDbContex.cs
@@ -0,0 +1,23 @@
+﻿using CodeFlow.Data.Context.Package.Configuration;
+using CodeFlow.Data.Context.Package.Tracking;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace CodeFlow.Data.Context.Package.Base.Context;
+
+public class BaseIdentityDbContex<TUser, TRole, TKey>(
+    DbContextOptions options) : IdentityDbContext<TUser, TRole, TKey>(options)
+        where TUser : IdentityUser<TKey>
+        where TRole : IdentityRole<TKey>
+        where TKey : IEquatable<TKey>
+{
+    public DbSet<CommandFailure> CommandFailures { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfiguration(new CommandFailureConfiguration());
+    }
+}
